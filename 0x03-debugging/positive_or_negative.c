201862d5c248c60abdda43137918702f248e1a03
titikorie <titikorie@gmail.com>
2023-03-24 02:26:51 -0700

    2 largest number

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include <main.h>
 /**
-  * positive_or_negative - checks forpositive or negative numbers
+  * positive_or_negative - checks for positive or negative numbers
   *
-  *0\: the number to be checked
-Return: always 0
+  *0i: the number to be checked
+  *
+  *Return: always 0
 */
+
 void positive_or_negative(int i);
 (
- if (i < 0)
- printf("%d is negative". i);
- else
- printf("%d is positive". i);
- )
+if (i < 0)
+printf("%d is negative". i);
+else
+printf("%d is positive". i);
+)
