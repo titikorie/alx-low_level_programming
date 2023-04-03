7cbe6996c6a78c9d4501ca38b034d2e31cfdccf0
titikorie <titikorie@gmail.com>
2023-04-03 05:36:15 -0700

    done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @needle: Substring to be located.
+ * @haystack: Location of substring @needle.
+ * Return: Pointer to the beginning of located substring.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			while (haystack[i] == needle[i])
+			{
+				if (needle[i + 1] == '\0')
+				{
+					return (haystack);
+				}
+				i++;
+			}
+		}
+		haystack++;
+	}
+	return ('\0');
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+7 pointers arrays string
